1c29422389c248d3dab6e658a63dc06835e4612d
FrancescoCaponero <francescocaponero1@gmail.com>
2023-01-29 12:23:23 +0100

    changed app to script

--- functions.php
+++ functions.php
@@ -7,8 +7,8 @@
 
 
 function CapoTheme_scripts_and_styles() {
-        wp_register_script( 'app', get_template_directory_uri() . '/js/app.js', array('jquery'), '1.0.0', 'all');
-        wp_enqueue_script( 'app' );
+        wp_register_script( 'script', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0.0', 'all');
+        wp_enqueue_script( 'script' );
         wp_register_style( 'style', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all' );
         wp_enqueue_style( 'style' );
 }
